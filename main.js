e61af0877d5329af2397881c3d570882203084ee
Tuan-Dang97 <tuandqf8@fullstack.edu.vn>
2025-06-19 22:19:30 +0700

    Hello

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-const
+const checkAll = document.querySelector('#checkAll');
+const itemCheck = document.querySelectorAll('.item-checkbox');
+const countCheck = document.querySelector('#statusText');
+
